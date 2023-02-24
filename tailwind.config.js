960a252ca9cc4c23c907c32ff8bec9fc6f72134b
Максим <53973885+imartin-ff@users.noreply.github.com>
2023-02-24 16:08:33 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F9F8FF',
+        'dark': '#232323',
+        'orange': '#EA5E20',
+        'gray': '#393C41',
+        'light-gray': "#7F889B",
+        'danger': "#FAE8EC",
+        'red': '#E82525'
+      },
+    },
+
+    container: {
+      center: true,
+      padding: "15px",
+      maxWidth: {
+        'xs': '100%',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '976px',
+        'xl': '1232px',
+        '2xl': '1232px',
+      },
+  },
+  },
+  plugins: [],
+}
